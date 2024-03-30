e6f3ac18536439a4b3255abb0fcd512346a1f6d6
Joseph Archibong <142097623+josepharchibong@users.noreply.github.com>
2024-03-30 20:09:19 +0100

    update readme.md

---
+++ SQL_Files/data_trasfrmation.sql
@@ -0,0 +1,8 @@
+SELECT	Gender, breakfast, calorie_per_day, coffee_picture, cooking_frequency, cuisine, 
+		current_diet_short, drink_picture, eating_changes_short, eating_out, employment_status, 
+		ethnic_food, exercise, father_education_level, fav_cuisine_grouped, fav_food, fries_picture,
+		fruit_per_day, grade_level, greek_food, do_you_feel_healthy, ideal_diet_short, income, indian_food,
+		italian_food, is_life_rewarding, marital_status, mother_education_level, nutritional_check, residence,
+		parents_cook, pay_for_meal_out, persian_food, self_perception_weight, soup_picture, sports, thai_food,
+		veggies_per_day, vitamins
+FROM dbo.food_choices
